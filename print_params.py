91f9b26cf6407d2e5de053ebea0eb39f974f4ce0
mitra <dimamarkov320@gmail.com>
2024-12-11 17:42:49 +0300

    Add files via upload

---
+++ print_params.py
@@ -0,0 +1,16 @@
+def print_params(a= 1, b= "строка", c= True):
+    print(a, b , c)
+
+print_params()
+print_params(10)
+print_params(10, 'новая строка')
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+values_list = [3.14, 'текст', False]
+values_dict = {'a': 42, 'b': 'слово', 'c': None}
+
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [2.2 , 'Lena']
+print_params(*values_list_2, 42)
